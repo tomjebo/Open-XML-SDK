68b460d2e2789077b44f347f58273155c0edc831
github-action-benchmark <github@users.noreply.github.com>
2023-06-27 05:01:29 +0000

    add Validation (benchmarkdotnet) benchmark result for 13341c09b0a50d0830ce1a58b1b771c32b116e19

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1687842089224,
+  "repoUrl": "https://github.com/dotnet/Open-XML-SDK",
+  "entries": {
+    "Validation": [
+      {
+        "commit": {
+          "author": {
+            "email": "tasou@microsoft.com",
+            "name": "Taylor Southwick",
+            "username": "twsouthwick"
+          },
+          "committer": {
+            "email": "tasou@microsoft.com",
+            "name": "Taylor Southwick",
+            "username": "twsouthwick"
+          },
+          "distinct": true,
+          "id": "13341c09b0a50d0830ce1a58b1b771c32b116e19",
+          "message": "valid",
+          "timestamp": "2023-06-26T21:55:53-07:00",
+          "tree_id": "278b75d64f04264c70c753f0fa418892c29e182b",
+          "url": "https://github.com/dotnet/Open-XML-SDK/commit/13341c09b0a50d0830ce1a58b1b771c32b116e19"
+        },
+        "date": 1687842065851,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "DocumentFormat.OpenXml.Benchmarks.ValidationTests.Validation",
+            "value": 87116642.22222222,
+            "unit": "ns",
+            "range": "± 814144.6662443189"
+          }
+        ]
+      }
+    ]
+  }
+}
