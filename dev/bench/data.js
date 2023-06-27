06220f230cd4d3e61879e9d3eda86fc815580df0
github-action-benchmark <github@users.noreply.github.com>
2023-06-27 15:29:21 +0000

    add Validation (benchmarkdotnet) benchmark result for 51d05949db03b8861f430689678d124da36ec3d5

---
+++ dev/bench/data.js
@@ -0,0 +1,34 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1687879760699,
+  "repoUrl": "https://github.com/dotnet/Open-XML-SDK",
+  "entries": {
+    "Validation": [
+      {
+        "commit": {
+          "author": {
+            "name": "dotnet",
+            "username": "dotnet"
+          },
+          "committer": {
+            "name": "dotnet",
+            "username": "dotnet"
+          },
+          "id": "51d05949db03b8861f430689678d124da36ec3d5",
+          "message": "Only update benchmark results on main",
+          "timestamp": "2023-06-26T08:41:16Z",
+          "url": "https://github.com/dotnet/Open-XML-SDK/pull/1461/commits/51d05949db03b8861f430689678d124da36ec3d5"
+        },
+        "date": 1687879752654,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "DocumentFormat.OpenXml.Benchmarks.ValidationTests.Validation",
+            "value": 82293123.80952382,
+            "unit": "ns",
+            "range": "± 1222927.1366732018"
+          }
+        ]
+      }
+    ]
+  }
+}
